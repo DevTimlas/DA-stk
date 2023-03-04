cf1a9ecf9de77609603d2c15ae9824458dd11b55
Timilehin <timilehinoladejo@yahoo.com>
2023-03-04 06:27:17 +0100

    Update mainraw.py

--- mainraw.py
+++ mainraw.py
@@ -250,8 +250,8 @@ async def sse(request):
 
 	
 if __name__ == "__main__":
-	uvicorn.run(app, host='0.0.0.0', port=os.getenv("PORT", default=5000), log_level="info")
-	# uvicorn.run("mainraw:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
+	# uvicorn.run(app, host='0.0.0.0', port=os.getenv("PORT", default=5000), log_level="info")
+	uvicorn.run("mainraw:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
 	
 	# print(dc)
 # print("Check file \"last-results.txt\", list of stocks to buy for the short term!")
